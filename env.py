4f16a82adf8c043779a5023140d369ba9486fdda
Andrew Shi <andrewpianoluv@gmail.com>
2025-02-28 08:47:07 -0800

    refactor

---
+++ env.py
@@ -0,0 +1,234 @@
+import rvo2
+import torch
+import itertools
+import numpy as np
+from numpy.linalg import norm
+from agent import Human, Robot
+from matplotlib import animation
+import matplotlib.pyplot as plt
+plt.rcParams['animation.ffmpeg_path'] = '/usr/bin/ffmpeg'
+
+class CrowdEnv(object):
+    def __init__(self):
+        self.human_list = []
+        self.circle_radius = 4
+        self.square_width = 10
+        self.discomfort_dist = 0.2
+        self.sim_time = 0
+        self.time_out_duration = 25
+        self.speed_samples = 5
+        self.rotation_samples = 16
+        # for orca simulation
+        self.safety_space = 0 # no safety space for human
+        self.neighbor_dist = 10
+        self.max_neighbors = 10
+        self.time_horizon = 5
+        self.time_horizon_obst = 5
+        self.time_step = 0.25
+        self.radius = 0.3
+        self.max_speed = 1
+        params = self.neighbor_dist, self.max_neighbors, self.time_horizon, self.time_horizon_obst
+        self.sim = rvo2.PyRVOSimulator(self.time_step, *params, self.radius, self.max_speed)
+        self.vel_samples = self.action_space()
+        #self.obs = [] # for render
+
+    def generate_human_postion(self, human_num, rule):
+        if  rule == "square":
+            self.square_rule(human_num)
+        elif rule == "circle":
+            self.circle_rule(human_num)
+
+    def circle_rule(self, human_num):
+        while True:
+            human = Human(self.time_step)
+            angle = np.random.random() * np.pi *2
+            px_noise = (np.random.random() - 0.5) * human.v_pref
+            py_noise = (np.random.random() - 0.5) * human.v_pref
+            px = self.circle_radius * np.cos(angle) + px_noise
+            py = self.circle_radius * np.sin(angle) + py_noise
+
+            collide = False
+            #print(len([self.robot] + self.human_list))
+            for agent in [self.robot] + self.human_list:
+                min_dist = human.radius + agent.radius + self.discomfort_dist
+                #print(min_dist)
+                if norm((px - agent.px, py - agent.py)) < min_dist or \
+                    norm((px - agent.gx, py - agent.gy)) < min_dist:
+                    collide = True
+                    break
+            if not collide:
+                human.set(px, py, -px, -py, 0, 0, 0)
+                self.human_list.append(human)
+
+            if len(self.human_list) >= human_num:
+                break
+
+    def square_rule(self, human_hum):
+        while True:
+            human = Human(self.time_step)
+            px = (np.random.random() - 0.5) * self.square_width
+            py = (np.random.random() - 0.5) * self.square_width
+            collide = False
+            for agent in [self.robot] + self.human_list:
+                min_dist = human.radius + agent.radius + self.discomfort_dist
+                if norm((px - agent.px, py - agent.py)) < min_dist or \
+                    norm((px - agent.gx, py - agent.gy)) < min_dist:
+                    collide = True
+                    break
+            if not collide:
+                human.set(px, py, -px, -py, 0, 0, 0)
+                self.human_list.append(human)
+
+            if len(self.human_list) >= human_hum:
+                break
+
+    def action_space(self):
+        speeds = [(np.exp((i + 1) / self.speed_samples) - 1) / (np.e - 1) * self.max_speed for i in range(self.speed_samples)]
+        rotations = np.linspace(0, 2 * np.pi, self.rotation_samples, endpoint=False)
+        action_space = []
+        for rotation, speed in itertools.product(rotations, speeds):
+            action_space.append([speed * np.cos(rotation), speed * np.sin(rotation)])
+        return action_space
+
+    def reset(self, human_num, test_phase=False, counter=None):
+        self.robot = Robot(self.time_step)
+        self.human_list = []
+        # if test_phase:
+        #     np.random.seed(counter)
+        self.generate_human_postion(human_num=human_num, rule="circle")
+        obs = [self.robot.full_state()] + [human.observable_state() for human in self.human_list]
+        assert len(obs) == 6 #debug
+        # orca simultion
+        params = self.neighbor_dist, self.max_neighbors, self.time_horizon, self.time_horizon_obst
+        for human in self.human_list:
+            self.sim.addAgent((human.px, human.py), *params, human.radius + 0.01 + self.safety_space,
+                              human.v_pref, (human.vx, human.py))
+        self.sim_time = 0
+        self.dg = norm(np.array(self.robot.get_position()) - np.array(self.robot.get_goal_position()))
+        #self.obs = obs
+        return obs
+
+    def step(self, action):
+        for i, human in enumerate(self.human_list):
+            v_pref  = np.array((human.gx, human.gy)) - np.array((human.px, human.py))
+            if norm(v_pref) > 1:
+                v_pref /= norm(v_pref)
+            self.sim.setAgentPrefVelocity(i, tuple(v_pref))
+            human.v_pref = v_pref
+
+        self.sim.doStep()
+        for i, human in enumerate(self.human_list):
+            human.set_position(self.sim.getAgentPosition(i))
+            human.set_velocity(self.sim.getAgentVelocity(i))
+        self.robot.step(action)
+        self.sim_time += self.time_step
+        # compute reward
+        distance_list = []
+        for human in self.human_list:
+            distance_list.append(norm(np.array(human.get_position()) - np.array(self.robot.get_position())) - 2 * self.radius)
+        d_min = min(distance_list)
+        current_dg = norm(np.array(self.robot.get_position()) - np.array(self.robot.get_goal_position()))
+        reaching_goal = current_dg < self.robot.radius
+
+        delta_d = self.dg - current_dg
+        self.dg = current_dg
+
+        if self.sim_time >= self.time_out_duration:
+            reward = 0
+            done = True
+            info = "timeout"
+        elif d_min < 0:
+            reward = -10
+            done = True
+            info = "collide"
+        elif d_min < self.discomfort_dist:
+            reward = 10*(d_min - self.discomfort_dist)
+            #print("penalty", reward)
+            done = False
+            info = "close"
+        elif reaching_goal:
+            reward = 10
+            done = True
+            info = "Goal, time {}".format(self.sim_time)
+        else:
+            reward = delta_d
+            #print("delta", delta_d)
+            done = False
+            info = "Onway"
+
+        obs = [self.robot.full_state()] + [human.observable_state() for human in self.human_list]
+        #self.obs = obs
+        return obs, reward, done, info
+    
+    def convert_coord(self, obs):
+        assert len(obs) == 6
+        # Use torch.tensor with dtype=torch.float32 to ensure consistent type
+        robot_state = torch.tensor(obs[0], dtype=torch.float32)
+        
+        # Convert human_state to numpy array first, then to tensor with float32 dtype
+        human_state = torch.tensor(np.array(obs[1:]), dtype=torch.float32)
+        
+        # Make sure human_state has the right shape
+        assert human_state.shape[0] == 5
+        assert human_state.shape[1] == 5
+        
+        dx = robot_state[5] - robot_state[0]
+        dy = robot_state[6] - robot_state[1]
+        dg = torch.tensor(norm((dx.item(), dy.item())), dtype=torch.float32).expand(5, 1)
+        rot = torch.atan2(dy, dx)
+        rot_expand = rot.expand(5, 1)
+        v_pref = robot_state[7].expand(5, 1)
+        vx = (robot_state[2] * torch.cos(rot) + robot_state[3] * torch.sin(rot)).expand(5, 1)
+        vy = (robot_state[3] * torch.cos(rot) - robot_state[2] * torch.sin(rot)).expand(5, 1)
+        radius = robot_state[4].expand(5, 1)
+        
+        vx_human = (human_state[:, 2] * torch.cos(rot) + human_state[:, 3] * torch.sin(rot)).unsqueeze(1)
+        vy_human = (human_state[:, 3] * torch.cos(rot) - human_state[:, 2] * torch.sin(rot)).unsqueeze(1)
+        px_human = ((human_state[:, 0] - robot_state[0]) * torch.cos(rot) + (human_state[:, 1] - robot_state[1]) * torch.sin(rot)).unsqueeze(1)
+        py_human = ((human_state[:, 1] - robot_state[1]) * torch.cos(rot) - (human_state[:, 0] - robot_state[0]) * torch.sin(rot)).unsqueeze(1)
+        radius_human = human_state[:, 4].unsqueeze(1)
+        radius_sum = radius + radius_human
+        
+        # Calculate distances between robot and humans
+        distances = []
+        for i in range(human_state.shape[0]):
+            dist = norm((human_state[i, 0].item() - robot_state[0].item(), 
+                        human_state[i, 1].item() - robot_state[1].item()))
+            distances.append(dist)
+        
+        da = torch.tensor(distances, dtype=torch.float32).unsqueeze(1)
+        
+        new_state = torch.cat([dg, rot_expand, vx, vy, v_pref, radius, 
+                            px_human, py_human, vx_human, vy_human, 
+                            radius_human, da, radius_sum], dim=1).unsqueeze(0)
+        
+        return new_state  # add batch dim
+    
+    def render(self):
+        # robot_state = self.obs[0]
+        # human_states = self.obs[1:]
+        # cmap = plt.cm.get_cmap('hsv', 10)
+        # robot_color = 'yellow'
+        # goal_color = 'red'
+        # arrow_color = 'red'
+        # arrow_style = patches.ArrowStyle("->", head_length=4, head_width=2)
+        fig, ax = plt.subplots(figsize=(7, 7))
+        ax.set_xlim(-6, 6)
+        ax.set_ylim(-6, 6)
+        def init():
+            for human in self.human_list:
+                human_circle = plt.Circle(human.get_position(), human.radius, fill=False, color='b')
+                ax.add_artist(human_circle)
+            ax.add_artist(plt.Circle(self.robot.get_position(), self.robot.radius, fill=True, color='r'))
+            return ax
+
+        def update(i):
+            for human in self.human_list:
+                human_circle = plt.Circle(human.get_position(), human.radius, fill=False, color='b')
+                ax.add_artist(human_circle)
+            ax.add_artist(plt.Circle(self.robot.get_position(), self.robot.radius, fill=True, color='r'))
+            return ax
+        anim = animation.FuncAnimation(fig, update, init_func = init, \
+        frames = int(self.time_out_duration/self.time_step), blit = True)
+        plt.show()
+        plt.pause(0.0001)
---
+++ train.py
@@ -0,0 +1,196 @@
+import numpy as np
+from collections import deque
+import random
+import torch
+from torch import nn
+import os
+from env import CrowdEnv
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+
+class Network(nn.Module):
+    def __init__(self,robot_dim, human_dim, lstm_hidden_dim, num_actions):
+        super().__init__()
+        self.lstm_hidden_dim = lstm_hidden_dim
+        self.robot_dim = robot_dim
+
+        self.lstm = nn.LSTM(human_dim, lstm_hidden_dim, batch_first =True)
+        self.mlp = nn.Sequential(
+            nn.Linear(robot_dim + lstm_hidden_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_actions)
+        )
+
+    def forward(self, state):
+        size = state.shape
+        robot_state = state[:, 0, :self.robot_dim]
+        human_state = state[:, :, self.robot_dim:]
+
+        h0 = torch.zeros(1, size[0], self.lstm_hidden_dim)
+        c0 = torch.zeros(1, size[0], self.lstm_hidden_dim)
+        output, (hn, cn) = self.lstm(human_state, (h0, c0))
+        hn = hn.squeeze(0)
+        joint_state = torch.cat([robot_state, hn], dim=1)
+        q_value = self.mlp(joint_state)
+
+        return q_value
+
+class DQN:
+    def __init__(self, model_path, env, lr, batch_size, gamma, eps_decay, eps_start, eps_end, initial_memory, memory_size):
+
+        self.env = env
+        self.model_path = model_path
+        self.lr = lr
+        self.gamma = gamma
+        self.eps_decay = eps_decay
+        self.eps_start = eps_start
+        self.eps_end = eps_end
+        self.initial_memory = initial_memory
+
+        self.replay_buffer = deque(maxlen=memory_size)
+        self.replay_buffer_b = deque(maxlen=memory_size)
+        self.batch_size = batch_size
+        self.num_actions = 80
+        self.robot_dim = 6
+        self.human_dim = 7
+        self.lstm_hidden_dim = 48
+        self.model = self.make_model()
+        self.first = False # for debug
+
+    def make_model(self):
+        model = Network(self.robot_dim, self.human_dim, self.lstm_hidden_dim, self.num_actions)
+        return model
+
+    def agent_policy(self, state, epsilon):
+        # epsilon greedy policy
+        if np.random.rand() < epsilon:
+            action = random.randrange(self.num_actions)
+        else:
+            q_value = self.model(state)
+            action = np.argmax(q_value.detach().numpy())
+
+        return action
+
+    def add_to_replay_buffer(self, state, action, reward, next_state, terminal):
+        self.replay_buffer.append((state, action, reward, next_state, terminal))
+
+    def sample_from_reply_buffer(self):
+        random_sample = random.sample(self.replay_buffer, self.batch_size)
+        random_sample_b = random.sample(self.replay_buffer_b, int(self.batch_size/2))
+        return random_sample + random_sample_b
+
+    def get_memory(self, random_sample):
+        states = torch.cat([i[0] for i in random_sample], dim=0)
+        actions = torch.tensor([i[1] for i in random_sample])
+        rewards = torch.tensor([i[2] for i in random_sample])
+        next_states = torch.cat([i[3] for i in random_sample], dim=0)
+        terminals = torch.tensor([i[4] for i in random_sample]) * 1
+
+        return states, actions, rewards, next_states, terminals
+
+    def train_with_relay_buffer(self):
+        # replay_memory_buffer size check
+        if len(self.replay_buffer) < self.batch_size:
+            return
+
+        sample = self.sample_from_reply_buffer()
+        states, actions, rewards, next_states, terminals = self.get_memory(sample)
+        next_q_mat = self.model(next_states)
+
+        next_q_vec = np.max(next_q_mat.detach().numpy(), axis=1).squeeze()
+
+        target_vec = rewards + self.gamma * next_q_vec * (1 - terminals.detach().numpy())
+        q_mat = self.model(states)
+        q_vec = q_mat.gather(dim=1, index=actions.unsqueeze(1)).type(torch.FloatTensor)
+        target_vec = target_vec.unsqueeze(1).type(torch.FloatTensor)
+        loss = self.loss_func(q_vec, target_vec)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    def train(self, num_episodes=2000):
+        self.model.train()
+        self.loss_func = nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        steps_done = 0
+        losses = []
+        rewards_list = []
+        for episode in range(num_episodes):
+            obs = env.reset(human_num=5, test_phase=False, counter=None)
+            state = env.convert_coord(obs)
+            reward_for_episode = 0
+            num_step_per_eps = 0
+            ep_buffer = []
+            while True:
+                epsilon = self.eps_end + (self.eps_start - self.eps_end) * np.exp(- steps_done / self.eps_decay)
+                received_action = self.agent_policy(state, epsilon)
+                vel_action = env.vel_samples[received_action]
+                steps_done += 1
+                num_step_per_eps += 1
+                next_obs, reward, terminal, info = env.step(vel_action)
+                next_state = env.convert_coord(next_obs)
+                # Store the experience in replay memory
+                #self.add_to_replay_buffer(state, received_action, reward, next_state, terminal)
+                ep_buffer.append((state, received_action, reward, next_state, terminal))
+                # add up rewards
+                reward_for_episode += reward
+                state = next_state
+
+                if max(len(self.replay_buffer), len(self.replay_buffer_b)) >= self.initial_memory and steps_done % 4 == 0:
+                    loss = self.train_with_relay_buffer()
+                    losses.append(loss.item())
+
+                if steps_done % 10000 == 0:
+                    plot_stats(steps_done, rewards_list, losses, episode)
+                    path = os.path.join(self.model_path, f"ep_{episode}.pth")
+                    torch.save(self.model.state_dict(), path)
+
+                if max(len(self.replay_buffer), len(self.replay_buffer_b)) >= self.initial_memory and not self.first:
+                    print("Start learning from buffer")
+                    print(len(self.replay_buffer))
+                    print(len(self.replay_buffer_b))
+                    self.first = True
+                if terminal:
+                    rewards_list.append(reward_for_episode)
+                    if info == "collide":
+                        self.replay_buffer_b += ep_buffer
+                    else:
+                        self.replay_buffer += ep_buffer
+                    print("Episode: {} done, Reward: {}, Status: {}".format(episode, reward_for_episode, info))
+                    break
+
+
+
+def plot_stats(frame_idx, rewards, losses, step):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title(f'Total frames {frame_idx}. Avg reward over last 10 episodes: {np.mean(rewards[-10:])}')
+    plt.plot(rewards)
+    plt.subplot(132)
+    plt.title('loss')
+    plt.plot(losses)
+    #plt.show()
+    plt.savefig('figures2/fig_{}.png'.format(step))
+
+
+if __name__ == "__main__":
+    env = CrowdEnv()
+
+    # setting up params
+    lr = 0.0001
+    batch_size = 64
+    eps_decay = 50000
+    eps_start = 0.5
+    eps_end = 0.1
+    initial_memory = 10000
+    memory_size = 10 * initial_memory
+    gamma = 0.9
+    num_episodes = 10000
+    model_path = "weights2/"
+    print('Start training')
+    model = DQN(model_path, env, lr, batch_size, gamma, eps_decay, eps_start, eps_end,initial_memory, memory_size)
+    model.train(num_episodes)
